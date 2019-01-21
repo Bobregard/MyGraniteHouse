29bac56e3f24fa37ccfec683e0eceef5b05e685f
Bobregard <b.pironkov@yahoo.com>
2019-01-21 15:43:49 +0200

    Added ViewModel and ProductsController

---
+++ MyGraniteHouse/ViewModels/ProductsViewModel.cs
@@ -0,0 +1,17 @@
+﻿using MyGraniteHouse.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyGraniteHouse.ViewModels
+{
+    public class ProductsViewModel
+    {
+        public Product Product { get; set; }
+
+        public IEnumerable<ProductTypes> ProductTypes { get; set; }
+
+        public IEnumerable<SpecialTags> SpecialTags { get; set; }
+    }
+}
