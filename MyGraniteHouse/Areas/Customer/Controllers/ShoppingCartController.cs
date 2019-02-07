b86e3e6d692d1c2c4ee60b79f17c717d36b8833e
Bobregard <b.pironkov@yahoo.com>
2019-02-07 02:17:13 +0200

    Added ShoppingCart Controller and Index action

---
+++ MyGraniteHouse/Areas/Customer/Controllers/ShoppingCartController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MyGraniteHouse.Data;
+using MyGraniteHouse.Extensions;
+using MyGraniteHouse.Models;
+using MyGraniteHouse.ViewModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace MyGraniteHouse.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class ShoppingCartController : Controller
+    {
+        private readonly ApplicationDbContext db;
+
+        [BindProperty]
+        public ShoppingCartViewModel ShoppingCartVM { get; set; }
+
+
+        public ShoppingCartController(ApplicationDbContext db)
+        {
+            this.db = db;
+            ShoppingCartVM = new ShoppingCartViewModel()
+            {
+                Products = new List<Product>()
+            };
+
+
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<int> listShoppingCart = HttpContext.Session.Get<List<int>>("ssShoppingCart");
+            if (listShoppingCart.Count > 0)
+            {
+                foreach (var cartItem in listShoppingCart)
+                {
+                    Product prod = this.db.Products.Include(p => p.SpecialTags).Include(p => p.ProductTypes).Where(p => p.Id == cartItem).FirstOrDefault();
+                    ShoppingCartVM.Products.Add(prod);
+                }
+            }
+
+            return View(ShoppingCartVM);
+        }
+    }
+}
