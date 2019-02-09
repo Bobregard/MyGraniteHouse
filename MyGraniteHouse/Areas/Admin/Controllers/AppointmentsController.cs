a3d4ac9c379351b588f5390fac206bfec1cc7e8c
Bobregard <b.pironkov@yahoo.com>
2019-02-09 12:31:16 +0200

    Edited appointments model and added appointments ViewModel and Controller

---
+++ MyGraniteHouse/Areas/Admin/Controllers/AppointmentsController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyGraniteHouse.Data;
+using MyGraniteHouse.Models;
+using MyGraniteHouse.Utility;
+using MyGraniteHouse.ViewModels;
+
+namespace MyGraniteHouse.Areas.Admin.Controllers
+{
+    [Authorize(Roles = StaticDetails.AdminEndUser + "," + StaticDetails.SuperAdminEndUser)]
+    [Area("Admin")]
+    public class AppointmentsController : Controller
+    {
+        private readonly ApplicationDbContext db;
+
+        public AppointmentsController(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            ClaimsPrincipal currentUser = this.User;
+            var claimsIdentity = (ClaimsIdentity)this.User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            AppointmentViewModel appointmentVM = new AppointmentViewModel()
+            {
+                Appointments = new List<Appointment>()
+            };
+
+            appointmentVM.Appointments = db.Appointments.Include(a => a.SalesPerson).ToList();
+            if (User.IsInRole(StaticDetails.AdminEndUser))
+            {
+                appointmentVM.Appointments = appointmentVM.Appointments.Where(a => a.SalesPersonId == claim.Value).ToList();
+            }
+
+            return View(appointmentVM);
+        }
+    }
+}
---
+++ MyGraniteHouse/ViewModels/AppointmentViewModel.cs
@@ -0,0 +1,13 @@
+﻿using MyGraniteHouse.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyGraniteHouse.ViewModels
+{
+    public class AppointmentViewModel
+    {
+        public List<Appointment> Appointments { get; set; }
+    }
+}
